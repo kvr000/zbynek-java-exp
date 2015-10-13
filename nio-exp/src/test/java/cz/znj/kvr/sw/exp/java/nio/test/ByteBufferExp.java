6a2a44f492916fcae8b65946eabf42001a445d21
kvr <kvr@centrum.cz>
2015-10-13 09:56:29 +0200

    bytebuffer tests

---
+++ nio-exp/src/test/java/cz/znj/kvr/sw/exp/java/nio/test/ByteBufferExp.java
@@ -0,0 +1,28 @@
+package cz.znj.kvr.sw.exp.java.nio.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+
+/**
+ * Created by vyskovsz on 09/10/2015.
+ */
+public class ByteBufferExp
+{
+	@Test
+	public void                     testRewinds()
+	{
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		buffer.putInt(0x12345678);
+		ByteBuffer extended = ByteBuffer.allocate(1024);
+		Assert.assertEquals(0, extended.position());
+		buffer.flip();
+		buffer.get();
+		extended.put(buffer);
+		Assert.assertEquals(3, extended.position());
+		extended.put((byte)0x69);
+		Assert.assertEquals(4, extended.position());
+	}
+}

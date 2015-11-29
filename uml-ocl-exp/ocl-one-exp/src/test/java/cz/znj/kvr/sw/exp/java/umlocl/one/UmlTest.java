d22271e76a22e746b18441c53662a6c967b5e70d
kvr <kvr@centrum.cz>
2015-11-29 22:01:33 +0100

    uml parsing experiments

---
+++ uml-ocl-exp/ocl-one-exp/src/test/java/cz/znj/kvr/sw/exp/java/umlocl/one/UmlTest.java
@@ -0,0 +1,19 @@
+package cz.znj.kvr.sw.exp.java.umlocl.one;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.util.concurrent.SettableListenableFuture;
+
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+
+public class UmlTest
+{
+	@Test
+	public void			testRead()
+	{
+	}
+}

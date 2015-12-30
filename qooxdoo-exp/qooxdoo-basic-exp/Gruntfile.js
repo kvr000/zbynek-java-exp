f60c1e6300f3e1183b873b7e27b1509ee1ede9a1
kvr <kvr@centrum.cz>
2015-12-30 22:33:36 +0100

    grunt setup

---
+++ qooxdoo-exp/qooxdoo-basic-exp/Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-typescript');
+	grunt.loadNpmTasks('grunt-ts');
+
+	grunt.initConfig({
+		jshint: {
+			files: ['Gruntfile.js', 'src/main/js/**/*.js','src/test/js/**/*.js']
+		},
+
+		typescript: {
+			base: {
+				src: ['src/main/typescript/**/*.ts'],
+				dest: 'src/main/js/',
+				options: {
+					module: 'amd', //or commonjs 
+					target: 'es5', //or es3 
+					//basePath: 'src/main/typescript',
+					sourceMap: true,
+					declaration: true
+				}
+			}
+		},
+
+		ts: {
+			base: {
+				src: [ 'src/main/typescript/**/*.ts' ],
+				dest: 'src/main/js/',
+				//options: {
+				//	module: 'amd', //or commonjs 
+				//	target: 'es5', //or es3 
+				//	//basePath: 'src/main/typescript',
+				//	sourceMap: true,
+				//	declaration: true
+				//}
+			}
+		},
+	});
+	grunt.registerTask('default', [ 'jshint' ]);
+	grunt.registerTask('compile', [ 'ts' ]);
+};

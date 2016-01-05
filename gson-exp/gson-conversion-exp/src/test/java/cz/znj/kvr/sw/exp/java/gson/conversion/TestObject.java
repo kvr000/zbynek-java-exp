9fddb0a7aca20a90bbd0cb9bd1afab0731687f54
Zbynek Vyskovsky <kvr000@gmail.com>
2016-01-05 20:24:37 +0100

    gson tests

---
+++ gson-exp/gson-conversion-exp/src/test/java/cz/znj/kvr/sw/exp/java/gson/conversion/TestObject.java
@@ -0,0 +1,28 @@
+package cz.znj.kvr.sw.exp.java.gson.conversion;
+
+public class TestObject
+{
+	public int getTestId()
+	{
+		return testId;
+	}
+
+	public void setTestId(int testId)
+	{
+		this.testId = testId;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	private int			testId;
+
+	private String			name;
+}

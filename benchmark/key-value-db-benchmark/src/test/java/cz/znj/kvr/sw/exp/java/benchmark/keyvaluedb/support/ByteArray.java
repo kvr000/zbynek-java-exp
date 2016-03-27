b1938e6c1050de04d26fb6e8959f3b8d30cab34f
Zbynek Vyskovsky <kvr000@gmail.com>
2016-03-27 22:23:43 +0200

    key-value map benchmarks

---
+++ benchmark/key-value-db-benchmark/src/test/java/cz/znj/kvr/sw/exp/java/benchmark/keyvaluedb/support/ByteArray.java
@@ -0,0 +1,33 @@
+package cz.znj.kvr.sw.exp.java.benchmark.keyvaluedb.support;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+/**
+ * Byte array wrapper, with simple hash and equals.
+ */
+public class ByteArray implements Serializable
+{
+	private final byte[] array;
+
+	public ByteArray(byte[] array) {
+		this.array = array;
+	}
+
+	@Override
+	public int hashCode() {
+		int h = 3;
+		for (int i = 0; i < array.length; ++i) {
+			h = h*17+array[i];
+		}
+		return h;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof ByteArray) {
+			return Arrays.equals(((ByteArray) o).array, this.array);
+		}
+		return false;
+	}
+}
---
+++ benchmark/key-value-db-benchmark/src/test/java/cz/znj/kvr/sw/exp/java/benchmark/keyvaluedb/support/MultiThreadedPopulator.java
@@ -0,0 +1,52 @@
+package cz.znj.kvr.sw.exp.java.benchmark.keyvaluedb.support;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.time.StopWatch;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Function;
+
+/**
+ * Database populator.
+ */
+@Log4j2
+public class MultiThreadedPopulator implements Populator
+{
+	public void populate(long numItems, Function<Long, CloseableConsumer<Long>> writerSupplier) {
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+
+		int processors = Runtime.getRuntime().availableProcessors();
+
+		ExecutorService executor = Executors.newFixedThreadPool(processors);
+		List<Future<Void>> futures = new ArrayList<>();
+		for (int p = 0; p < processors; ++p) {
+			int cp = p;
+			futures.add(executor.submit(() -> {
+				try (CloseableConsumer<Long> writer = writerSupplier.apply((long)cp)) {
+					for (long i = cp; i < numItems; i += processors) {
+						writer.accept(i);
+					}
+				}
+				return (Void)null;
+			}));
+		}
+		futures.forEach((Future<Void> future) -> {
+			try {
+				future.get();
+			}
+			catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+			catch (ExecutionException e) {
+				throw new RuntimeException(e);
+			}
+		});
+		log.info("Populating done in "+stopWatch.getTime()+" ms");
+	}
+}

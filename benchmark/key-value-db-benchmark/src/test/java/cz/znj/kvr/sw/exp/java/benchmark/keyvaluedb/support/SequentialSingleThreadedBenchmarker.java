b56f6aa7686cc70dd03229fee0298eded89e1de1
Zbynek Vyskovsky <kvr000@gmail.com>
2016-04-30 23:40:33 +0200

    key-value map benchmarks

---
+++ benchmark/key-value-db-benchmark/src/test/java/cz/znj/kvr/sw/exp/java/benchmark/keyvaluedb/support/SequentialSingleThreadedBenchmarker.java
@@ -0,0 +1,25 @@
+package cz.znj.kvr.sw.exp.java.benchmark.keyvaluedb.support;
+
+import org.apache.commons.lang3.RandomUtils;
+
+import java.io.IOException;
+import java.util.function.Function;
+
+/**
+ * Sequential, single threaded, benchmarker.
+ */
+public class SequentialSingleThreadedBenchmarker implements Benchmarker
+{
+	@Override
+	public void benchmark(long steps, long numItems, Function<Long, CloseableConsumer<Long>> writerSupplier) {
+		long start = RandomUtils.nextLong(0, numItems-steps);
+		try (CloseableConsumer<Long> writer = writerSupplier.apply(0L)) {
+			for (long i = 0; i < steps; ++i) {
+				writer.accept(start+i);
+			}
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}

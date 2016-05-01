329d917dd24b3cad6a7ba7c561f18c6be49851b9
Zbynek Vyskovsky <kvr000@gmail.com>
2016-05-01 21:31:22 +0200

    Streams null tests

---
+++ corejava-exp/corejava-stream-exp/src/test/java/cz/znj/kvr/sw/exp/java/corejava/stream/NullTest.java
@@ -0,0 +1,58 @@
+package cz.znj.kvr.sw.exp.java.corejava.stream;
+
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+/**
+ *
+ */
+public class NullTest
+{
+	@Test
+	public void testNullInOriginal()
+	{
+		Function<Integer, Integer> mapper = mock(Function.class);
+		Predicate<Integer> filter = mock(Predicate.class);
+
+		when(mapper.apply(any()))
+				.thenAnswer((InvocationOnMock inv) -> inv.getArgument(0));
+		when(filter.test(any()))
+				.thenAnswer((InvocationOnMock inv) -> true);
+
+		List<Integer> result = Arrays.asList(1, null, 3).stream()
+				.map(mapper)
+				.filter(filter)
+				.collect(Collectors.toList());
+
+		verify(mapper, times(1))
+				.apply(1);
+		verify(mapper, times(1))
+				.apply(null);
+		verify(mapper, times(1))
+				.apply(3);
+
+		verify(filter, times(1))
+				.test(1);
+		verify(filter, times(1))
+				.test(null);
+		verify(filter, times(1))
+				.test(3);
+
+		Assert.assertEquals(result, Arrays.asList(1, null, 3));
+	}
+}

62beb9d1f33aafc0dd516986fd5daaaf55f1df5a
Zbynek Vyskovsky <kvr000@gmail.com>
2016-05-09 22:57:31 +0200

    Asynchronous DNS

--- nio-exp/niosocket-exp/src/test/java/cz/znj/kvr/sw/exp/java/nio/socket/test/PortForwarderRunner.java
+++ nio-exp/niosocket-exp/src/test/java/cz/znj/kvr/sw/exp/java/nio/socket/test/PortForwarderRunner.java
@@ -3,6 +3,8 @@ package cz.znj.kvr.sw.exp.java.nio.socket.test;
 import com.google.common.collect.ImmutableList;
 import cz.znj.kvr.sw.exp.java.nio.socket.forward.PortForwarder;
 
+import java.util.concurrent.Executors;
+
 
 /**
  * Proxy runner
@@ -11,7 +13,7 @@ public class PortForwarderRunner
 {
 	public static void main(String[] args) throws Exception
 	{
-		new PortForwarder()
+		new PortForwarder(Executors.newCachedThreadPool())
 			.runForwards(ImmutableList.of(
 				PortForwarder.ForwardConfig.builder()
 					.bindProto("tcp4")
@@ -20,6 +22,20 @@ public class PortForwarderRunner
 					.connectProto("tcp4")
 					.connectHost("localhost")
 					.connectPort(4444)
+					.build(),
+				PortForwarder.ForwardConfig.builder()
+					.bindProto("unix")
+					.bindPath("target/forward.socket")
+					.connectProto("tcp4")
+					.connectHost("localhost")
+					.connectPort(4444)
+					.build(),
+				PortForwarder.ForwardConfig.builder()
+					.bindProto("tcp4")
+					.bindHost("localhost")
+					.bindPort(2222)
+					.connectProto("unix")
+					.connectPath("target/forward.socket")
 					.build()
 			)).get();
 	}

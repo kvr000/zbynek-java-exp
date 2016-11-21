9849817c222b00ec10d9ab3be85b2f70db12e9e0
kvr <kvr@centrum.cz>
2016-11-20 22:03:15 -0800

    Spring Dependency Injection specials for unit tests

---
+++ spring-exp/spring-importdi-exp/src/test/java/cz/znj/kvr/sw/exp/java/spring/di/inject/InjectConfigTest.java
@@ -0,0 +1,83 @@
+package cz.znj.kvr.sw.exp.java.spring.di.inject;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+
+@ContextConfiguration(classes = { InjectConfigTest.MainConfig.class, InjectConfigTest.SecondConfig.class })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class InjectConfigTest
+{
+	@Inject
+	private String childBean;
+
+	@Inject
+	@Named("injectConfigTest.MainConfig")
+	private ConfigParent mainConfig;
+
+	@Inject
+	@Named("injectConfigTest.SecondConfig")
+	private ConfigParent secondConfig;
+
+	@Inject
+	@Named("cz.znj.kvr.sw.exp.java.spring.di.inject.InjectConfigTest$ChildConfig")
+	private ConfigParent childConfig;
+
+	@Test
+	public void testImport()
+	{
+		Assert.assertNotNull(childBean);
+		Assert.assertEquals("Hello World", childBean);
+	}
+
+	public static class ConfigParent
+	{
+	}
+
+	@Configuration
+	@Import(ChildConfig.class)
+	public static class MainConfig extends ConfigParent
+	{
+		@Bean
+		public String mainBean()
+		{
+			return "Hello";
+		}
+	}
+
+	@Configuration
+	@Import(ChildConfig.class)
+	public static class SecondConfig extends ConfigParent
+	{
+		@Bean
+		public String secondBean()
+		{
+			return "World";
+		}
+	}
+
+	@Configuration
+	public static class ChildConfig extends ConfigParent
+	{
+		@Inject
+		private String mainBean;
+
+		@Inject
+		private String secondBean;
+
+		@Bean
+		public String childBean()
+		{
+			return mainBean+" "+secondBean;
+		}
+	}
+}

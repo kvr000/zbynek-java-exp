01ea169c8b04bd2d4748d2a193a3134aa255f34d
Zbynek Vyskovsky <kvr000@gmail.com>
2016-12-02 20:23:41 -0800

    MongoDb Spring templates

---
+++ mongodb-exp/mongodb-basic-exp/src/test/java/cz/znj/kvr/sw/exp/java/mongodb/basic/config/MongoEmbeddedConfig.java
@@ -0,0 +1,43 @@
+package cz.znj.kvr.sw.exp.java.mongodb.basic.config;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.mongo.tests.MongodForTestsFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+
+/**
+ * @author
+ * 	Zbynek Vyskovsky
+ */
+@Configuration
+public class MongoEmbeddedConfig
+{
+	@PostConstruct
+	public MongoEmbeddedConfig init() throws IOException
+	{
+		mongodbFactory = new MongodForTestsFactory(Version.V3_3_1);
+		return this;
+	}
+
+	@PreDestroy
+	public void destroy()
+	{
+		mongodbFactory.shutdown();
+	}
+
+	@Bean
+	public MongoClient mongoClient() throws UnknownHostException
+	{
+		return mongodbFactory.newMongo();
+	}
+
+	private MongodForTestsFactory mongodbFactory;
+}

f74192218a1ffa1ba38d6638e918cc01b83b1de4
Zbynek Vyskovsky <kvr000@gmail.com>
2016-12-02 20:23:41 -0800

    MongoDb Spring templates

---
+++ guice-exp/guice-di-exp/src/test/java/cz/znj/kvr/sw/exp/java/guice/di/inject/SetterInjectTest.java
@@ -0,0 +1,51 @@
+package cz.znj.kvr.sw.exp.java.guice.di.inject;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import cz.znj.kvr.sw.exp.java.guice.di.common.First;
+import cz.znj.kvr.sw.exp.java.guice.di.common.Second;
+import cz.znj.kvr.sw.exp.java.guice.di.common.impl.FirstImpl;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.inject.Inject;
+
+
+public class SetterInjectTest
+{
+	@Test
+	public void testInject()
+	{
+		Module module = new TestModule();
+		Injector injector = Guice.createInjector(module);
+		Second bean = injector.getInstance(Second.class);
+		Assert.assertEquals(1, bean.getSecondValue());
+	}
+
+	public class TestModule extends AbstractModule
+	{
+		@Override
+		protected void configure() {
+			bind(First.class).to(FirstImpl.class);
+			bind(Second.class).to(MyBean.class);
+		}
+	}
+
+	public static class MyBean implements Second {
+		private First first;
+
+		@Inject
+		public void setFirst(First first) {
+			this.first = first;
+		}
+
+		public int getSecondValue() {
+			return first.getFirstValue();
+		}
+	}
+}

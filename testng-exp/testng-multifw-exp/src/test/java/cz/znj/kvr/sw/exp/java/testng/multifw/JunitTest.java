e0517d03087377b023721e027954d29ce124ffc4
Zbynek Vyskovsky <kvr000@gmail.com>
2016-12-03 22:25:04 -0800

    Multiple test frameworks experiment

---
+++ testng-exp/testng-multifw-exp/src/test/java/cz/znj/kvr/sw/exp/java/testng/multifw/JunitTest.java
@@ -0,0 +1,19 @@
+package cz.znj.kvr.sw.exp.java.testng.multifw;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.Test;
+
+
+/**
+ * @author
+ * 	Zbyněk Vyškovský
+ */
+@Log4j2
+public class JunitTest
+{
+	@Test
+	public void testJunit()
+	{
+		log.error("Hello from testJunit");
+	}
+}

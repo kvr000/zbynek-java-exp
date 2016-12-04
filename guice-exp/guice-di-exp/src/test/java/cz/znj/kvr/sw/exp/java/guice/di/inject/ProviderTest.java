f3dd0005bce1ba3690a1b265c123bf599ac026bb
Zbynek Vyskovsky <kvr000@gmail.com>
2016-12-03 22:37:11 -0800

    Guice providers

---
+++ guice-exp/guice-di-exp/src/test/java/cz/znj/kvr/sw/exp/java/guice/di/inject/ProviderTest.java
@@ -0,0 +1,50 @@
+package cz.znj.kvr.sw.exp.java.guice.di.inject;
+
+import com.google.inject.*;
+import cz.znj.kvr.sw.exp.java.guice.di.common.Facade;
+import cz.znj.kvr.sw.exp.java.guice.di.common.First;
+import cz.znj.kvr.sw.exp.java.guice.di.common.Second;
+import cz.znj.kvr.sw.exp.java.guice.di.common.impl.FacadeImpl;
+import cz.znj.kvr.sw.exp.java.guice.di.common.impl.FirstImpl;
+import cz.znj.kvr.sw.exp.java.guice.di.common.impl.SecondImpl;
+import lombok.Getter;
+import lombok.Setter;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.inject.Inject;
+
+
+public class ProviderTest
+{
+	@Test
+	public void testInject()
+	{
+		Module module = new TestModule();
+		Injector injector = Guice.createInjector(module);
+		MyBean bean = injector.getInstance(MyBean.class);
+		Assert.assertEquals(2, bean.getFacade().getSecond().getSecondValue());
+	}
+
+	public class TestModule extends AbstractModule
+	{
+		@Override
+		protected void configure() {
+			bind(First.class).to(FirstImpl.class);
+			bind(Second.class).to(SecondImpl.class);
+		}
+
+		@Provides
+		public Facade facade(First first, Second second)
+		{
+			return new FacadeImpl().setFirst(first).setSecond(second).init();
+		}
+	}
+
+	@Getter
+	public static class MyBean
+	{
+		@Inject
+		private Facade facade;
+	}
+}

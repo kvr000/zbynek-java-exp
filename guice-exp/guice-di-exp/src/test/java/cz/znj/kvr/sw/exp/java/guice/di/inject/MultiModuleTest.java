a0b5f24e592e9310794cdf2fcef14d751bd8d212
Zbynek Vyskovsky <kvr000@gmail.com>
2016-12-17 20:11:32 -0800

    Guice JSR-250 tests

---
+++ guice-exp/guice-di-exp/src/test/java/cz/znj/kvr/sw/exp/java/guice/di/inject/MultiModuleTest.java
@@ -0,0 +1,55 @@
+package cz.znj.kvr.sw.exp.java.guice.di.inject;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import cz.znj.kvr.sw.exp.java.guice.di.common.First;
+import cz.znj.kvr.sw.exp.java.guice.di.common.Second;
+import cz.znj.kvr.sw.exp.java.guice.di.common.impl.FirstImpl;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.inject.Inject;
+
+
+public class MultiModuleTest
+{
+	@Test
+	public void testInject()
+	{
+		Injector injector = Guice.createInjector(new FirstTestModule(), new SecondTestModule());
+		Second bean = injector.getInstance(Second.class);
+		Assert.assertEquals(1, bean.getSecondValue());
+	}
+
+	public class FirstTestModule extends AbstractModule
+	{
+		@Override
+		protected void configure() {
+			bind(First.class).to(FirstImpl.class);
+		}
+	}
+
+
+	public class SecondTestModule extends AbstractModule
+	{
+		@Override
+		protected void configure() {
+			bind(Second.class).to(MyBean.class);
+		}
+	}
+
+	public static class MyBean implements Second {
+		private First first;
+
+		@Inject
+		public void setFirst(First first) {
+			this.first = first;
+		}
+
+		public int getSecondValue() {
+			return first.getFirstValue();
+		}
+	}
+}

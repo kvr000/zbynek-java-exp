2a8729ebfeeb3372da48fb80df17ea584439a331
Zbynek Vyskovsky <kvr000@gmail.com>
2017-01-02 19:40:33 -0800

    Singleton Guice tests

---
+++ guice-exp/guice-di-exp/src/test/java/cz/znj/kvr/sw/exp/java/guice/di/inject/SingletonTest.java
@@ -0,0 +1,43 @@
+package cz.znj.kvr.sw.exp.java.guice.di.inject;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.Singleton;
+import cz.znj.kvr.sw.exp.java.guice.di.common.First;
+import cz.znj.kvr.sw.exp.java.guice.di.common.Second;
+import cz.znj.kvr.sw.exp.java.guice.di.common.impl.FirstImpl;
+import cz.znj.kvr.sw.exp.java.guice.di.common.impl.SecondImpl;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.inject.Inject;
+
+
+public class SingletonTest
+{
+	@Test
+	public void testSingleton()
+	{
+		Module module = new TestModule();
+		Injector injector = Guice.createInjector(module);
+
+		First first0 = injector.getInstance(First.class);
+		First first1 = injector.getInstance(First.class);
+		Assert.assertTrue("Non-singleton instances must be the same", first0 != first1);
+
+		Second second0 = injector.getInstance(Second.class);
+		Second second1 = injector.getInstance(Second.class);
+		Assert.assertTrue("Singleton instances must be the same", second0 == second1);
+	}
+
+	public class TestModule extends AbstractModule
+	{
+		@Override
+		protected void configure() {
+			bind(First.class).to(FirstImpl.class);
+			bind(Second.class).to(SecondImpl.class).in(Singleton.class);
+		}
+	}
+}

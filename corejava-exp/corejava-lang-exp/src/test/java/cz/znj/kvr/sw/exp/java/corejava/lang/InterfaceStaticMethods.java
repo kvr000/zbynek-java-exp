e5cf13a954b0328a69f3a37227618e282cc20e9b
Zbynek Vyskovsky <kvr000@gmail.com>
2018-12-22 22:21:31 -0800

    exception experiments

---
+++ corejava-exp/corejava-lang-exp/src/test/java/cz/znj/kvr/sw/exp/java/corejava/lang/InterfaceStaticMethods.java
@@ -0,0 +1,26 @@
+package cz.znj.kvr.sw.exp.java.corejava.lang;
+
+
+/**
+ * Combining factory methods into interface method (which is bad approach but may be needed for some work-arounds).
+ *
+ * @author
+ * 	Zbynek Vyskovsky
+ */
+public interface InterfaceStaticMethods
+{
+	public static final InterfaceStaticMethods instance = new InterfaceStaticMethods() {
+		@Override
+		public String greet(String name)
+		{
+			return "Hello";
+		}
+	};
+
+	public static InterfaceStaticMethods getInstance()
+	{
+		return instance;
+	}
+
+	String greet(String name);
+}

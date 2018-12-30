12ef81318f87fdf61fc6c8b5f1c090615707d4cf
Zbynek Vyskovsky <kvr000@gmail.com>
2018-12-29 19:18:44 -0800

    dagger experiments

---
+++ dagger-exp/dagger-di-exp/src/main/java/cz/znj/kvr/sw/exp/java/dagger/di/common/impl/FirstImpl.java
@@ -0,0 +1,19 @@
+package cz.znj.kvr.sw.exp.java.dagger.di.common.impl;
+
+import cz.znj.kvr.sw.exp.java.dagger.di.common.First;
+import lombok.AllArgsConstructor;
+
+import javax.inject.Inject;
+
+
+/**
+ * @author
+ * 	Zbynek Vyskovsky
+ */
+@AllArgsConstructor(onConstructor = @__(@Inject))
+public class FirstImpl implements First {
+	@Override
+	public int getFirstValue() {
+		return 1;
+	}
+}

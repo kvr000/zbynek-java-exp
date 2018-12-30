4f16ecce9a6f588d6f4c824d4bae684a6eb98c4a
Zbynek Vyskovsky <kvr000@gmail.com>
2018-12-29 19:18:44 -0800

    dagger experiments

---
+++ dagger-exp/dagger-di-exp/src/main/java/cz/znj/kvr/sw/exp/java/dagger/di/common/Second.java
@@ -0,0 +1,9 @@
+package cz.znj.kvr.sw.exp.java.dagger.di.common;
+
+/**
+ * @author
+ * 	Zbynek Vyskovsky
+ */
+public interface Second {
+	public int getSecondValue();
+}
---
+++ dagger-exp/dagger-di-exp/src/main/java/cz/znj/kvr/sw/exp/java/dagger/di/common/dagger/InterfaceImplModule.java
@@ -0,0 +1,41 @@
+package cz.znj.kvr.sw.exp.java.dagger.di.common.dagger;
+
+import cz.znj.kvr.sw.exp.java.dagger.di.common.Facade;
+import cz.znj.kvr.sw.exp.java.dagger.di.common.First;
+import cz.znj.kvr.sw.exp.java.dagger.di.common.Second;
+import cz.znj.kvr.sw.exp.java.dagger.di.common.impl.FacadeImpl;
+import cz.znj.kvr.sw.exp.java.dagger.di.common.impl.FirstImpl;
+import cz.znj.kvr.sw.exp.java.dagger.di.common.impl.SecondImpl;
+import dagger.Module;
+import dagger.Provides;
+
+import javax.inject.Singleton;
+
+
+/**
+ *
+ */
+@Module
+public class InterfaceImplModule
+{
+	@Singleton
+	@Provides
+	public static First first()
+	{
+		return new FirstImpl();
+	}
+
+	@Provides
+	@Singleton
+	public static Second second()
+	{
+		return new SecondImpl();
+	}
+
+	@Provides
+	@Singleton
+	public Facade facade(First first, Second second)
+	{
+		return new FacadeImpl(first, second);
+	}
+}

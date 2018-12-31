86ea0e94473430b27df57af780830b9c37e89075
Zbynek Vyskovsky <kvr000@gmail.com>
2018-12-30 18:41:32 -0800

    delayed class load experiment

---
+++ corejava-exp/corejava-lang-exp/src/test/java/cz/znj/kvr/sw/exp/java/corejava/lang/LoadTestClass.java
@@ -0,0 +1,13 @@
+package cz.znj.kvr.sw.exp.java.corejava.lang;
+
+
+/**
+ * The test is not thread safe, parallel testing must be disabled.
+ */
+public class LoadTestClass
+{
+	static
+	{
+		LoadTest.loaded = true;
+	}
+}

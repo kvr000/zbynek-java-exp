d46aabd77d15660c1196ab1de99d975e6186a00a
Zbynek Vyskovsky <kvr000@gmail.com>
2019-01-26 22:13:10 -0800

    Spring views experiments

---
+++ spring-exp/spring-jsp-exp/spring-jsp-intro/src/main/java/cz/znj/kvr/sw/exp/java/spring/jsp/intro/controller/HomeController.java
@@ -0,0 +1,30 @@
+package cz.znj.kvr.sw.exp.java.spring.jsp.intro.controller;
+
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+
+/**
+ *
+ */
+@Controller
+public class HomeController
+{
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Map<String, Object> model)
+	{
+		model.put("message", "Hello world");
+		model.put("command", new InputModel());
+		return "home";
+	}
+
+	@Data
+	public static class InputModel
+	{
+		private String message;
+	}
+}

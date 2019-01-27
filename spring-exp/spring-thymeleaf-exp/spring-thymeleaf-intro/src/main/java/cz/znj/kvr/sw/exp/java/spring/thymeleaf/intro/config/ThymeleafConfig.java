90856718b3799aff20d82c8af34c5faf9c31e50d
Zbynek Vyskovsky <kvr000@gmail.com>
2019-01-26 22:13:10 -0800

    Spring views experiments

---
+++ spring-exp/spring-thymeleaf-exp/spring-thymeleaf-intro/src/main/java/cz/znj/kvr/sw/exp/java/spring/thymeleaf/intro/config/ThymeleafConfig.java
@@ -0,0 +1,27 @@
+package cz.znj.kvr.sw.exp.java.spring.thymeleaf.intro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+
+/**
+ *
+ */
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public ClassLoaderTemplateResolver secondaryTemplateResolver() {
+        ClassLoaderTemplateResolver secondaryTemplateResolver = new ClassLoaderTemplateResolver();
+        secondaryTemplateResolver.setPrefix("thymeleaf/");
+        secondaryTemplateResolver.setSuffix(".html");
+        secondaryTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        secondaryTemplateResolver.setCharacterEncoding("UTF-8");
+        secondaryTemplateResolver.setOrder(1);
+        secondaryTemplateResolver.setCheckExistence(true);
+
+        return secondaryTemplateResolver;
+    }
+}

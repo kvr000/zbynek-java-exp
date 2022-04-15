d48b49ca540ffd32188fce721ca503ce32cb4823
Zbynek Vyskovsky <kvr000@gmail.com>
2022-04-14 18:31:01 -0700

    Java project template for exercises

---
+++ template/java-template/src/main/java/com/github/kvr000/template/java/Dummy.java
@@ -0,0 +1,9 @@
+package com.github.kvr000.template.java;
+
+public class Dummy
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+	}
+}
---
+++ template/java-template/src/test/java/com/github/kvr000/template/java/DummyTest.java
@@ -0,0 +1,13 @@
+package com.github.kvr000.template.java;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DummyTest
+{
+	@Test
+	public void testHello()
+	{
+		Assert.assertEquals("hello", "hello");
+	}
+}

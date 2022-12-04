3c8135f625e2a15dbcc58d62ef746597cf35b0dd
Zbynek Vyskovsky <kvr000@gmail.com>
2022-12-03 19:31:11 -0800

    Jar in jar classloader

---
+++ jar-exp/jar-in-jar/src/main/java/cz/znj/kvr/sw/exp/java/jar/jarinjar/LoadedMain.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2015 Zbynek Vyskovsky mailto:kvr000@gmail.com http://github.com/kvr000/zbynek-java/exp/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cz.znj.kvr.sw.exp.java.jar.jarinjar;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.jdotsoft.jarloader.JarClassLoader;
+
+import java.io.InputStream;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class LoadedMain
+{
+	public static void main(String[] args) throws Exception
+	{
+		ImmutableMap.of("key", "value");
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		try {
+			CompletableFuture.runAsync(() -> ImmutableList.of("list"), executor).get();
+			CompletableFuture.runAsync(() -> ImmutableSet.of("set")).get();
+			System.out.println("lsof exit: "+
+				new ProcessBuilder("lsof", "-p", String.valueOf(ProcessHandle.current().pid()))
+					.redirectOutput(ProcessBuilder.Redirect.INHERIT)
+					.redirectErrorStream(false)
+					.start()
+					.waitFor()
+			);
+			try (InputStream in = Main.class.getResourceAsStream("/META-INF/MANIFEST.MF")) {
+				in.transferTo(System.out);
+			}
+			try (InputStream in = Main.class.getResourceAsStream("/META-INF/maven/com.google.guava/guava/pom.properties")) {
+				in.transferTo(System.out);
+			}
+			System.out.println("OK");
+			System.exit(0);
+		}
+		finally {
+			executor.shutdown();
+		}
+	}
+}
---
+++ jar-exp/jar-in-jar/src/main/java/cz/znj/kvr/sw/exp/java/jar/jarinjar/Main.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2015 Zbynek Vyskovsky mailto:kvr000@gmail.com http://github.com/kvr000/zbynek-java/exp/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cz.znj.kvr.sw.exp.java.jar.jarinjar;
+
+import com.jdotsoft.jarloader.JarClassLoader;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.regex.Pattern;
+
+
+public class Main
+{
+	public static void main(String[] args) throws Exception, Throwable
+	{
+		{
+			String classpathStr = System.getProperty("java.class.path");
+			System.out.println(Arrays.asList(classpathStr.split(Pattern.quote(File.pathSeparator))));
+		}
+
+		JarClassLoader jcl = new JarClassLoader();
+		jcl.invokeMain(Main.class.getPackageName() + ".LoadedMain", args);
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		CompletableFuture.runAsync(() -> System.out.println("main main")).get();
+	}
+}

84a454e6410289e77105c5f5ced338b201a075db
Zbynek Vyskovsky <kvr000@gmail.com>
2024-08-08 19:59:57 +0200

    Fix order of shade and assembly plugins

--- spring-boot-exp/spring-boot-basic-webapp/src/main/java/com/github/kvr000/exp/java/springboot/basicwebapp/controller/HomeController.java
+++ spring-boot-exp/spring-boot-basic-webapp/src/main/java/com/github/kvr000/exp/java/springboot/basicwebapp/controller/HomeController.java
@@ -3,7 +3,8 @@ package com.github.kvr000.exp.java.springboot.basicwebapp.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@RestController("/")
 public class HomeController
 {
 	@GetMapping("/")
